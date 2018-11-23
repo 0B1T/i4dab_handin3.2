0ecb8acc56cb8dfba7b12136cf4d66d2895a4610
John Romby <au518762@uni.au.dk>
2018-11-23 10:04:13 +0100

    Add project files.

---
+++ ContactCatalog/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ContactCatalog.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
